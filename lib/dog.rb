69f9507079c0f2a60f71f0c0fd0efa4860e928e7
Warren Loy Harper II <warrenharper2@att.net>
2019-08-20 20:05:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,21 @@
 class Dog
-  def initialize(name)
-    @name = name
-    end
- 
+  attr_accessor :name, :breed
   def name
     @name
   end
+
+  def name=(name)
+    @name = name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+end
    def name=(new_name)
     @name = new_name
 end
-
-kanye = dog.new("fido")
-kanye.name
